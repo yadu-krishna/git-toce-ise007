0fc7ddd8f68369613496850b891759e46b012395
yadu-krishna <krishna.ykrr@gmail.com>
2025-04-04 15:25:30 +0530

    Add files via upload

---
+++ App.java
@@ -0,0 +1,13 @@
+package TOCE1.ise1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
